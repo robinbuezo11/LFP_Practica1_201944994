38dfa315712ba37ba298700e5ad15655140049ad
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2022-08-14 00:16:26 -0600

    [add] boton cargar archivo y accion prueba

--- Window.py
+++ Window.py
@@ -1,21 +1,29 @@
 import tkinter as tk
-
+from tkinter import messagebox as msgbx
 
 class Window(tk.Frame):
     def __init__(self,window) -> None:
-        super().__init__(window)
-        window.title('Practica 1')
-        window.geometry('850x650')
-        window.config(background='sky blue')
+        self.window = window
+        super().__init__(self.window)
+        self.window.title('Practica 1')
+        self.window.geometry('850x650')
+        self.window.config(background='sky blue')
 
-        self.lblcurso = tk.Label(window,text='Nombre del Curso: Lab. Lenguajes Formales y de Programación',font='Arial 16 bold', 
+        self.lblcurso = tk.Label(window,text='Lenguajes Formales y de Programación A+',font='Arial 16 bold', 
                                     background='sky blue')
         self.lblcurso.place(x=100, y=30)
 
-        self.lblstudent = tk.Label(window,text='Nombre del Estudiante: Robin Omar Buezo Díaz', font='Arial 16 bold', background='sky blue')
+        self.lblstudent = tk.Label(window,text='Robin Omar Buezo Díaz', font='Arial 16 bold', background='sky blue')
         self.lblstudent.place(x=100, y= 70)
 
-        self.lblcarne = tk.Label(window,text='Carné del Estudiante: 201944994', font='Arial 16 bold', background='sky blue')
+        self.lblcarne = tk.Label(window,text='201944994', font='Arial 16 bold', background='sky blue')
         self.lblcarne.place(x=100, y=110)
 
+        self.button = tk.Button(window, text='Cargar Archivo', width=20, height=2, background='SpringGreen3', font='Arial 12 bold',
+                                    command=self.actButton)
+        self.button.place(x=330, y=170)
+
         window.mainloop()
+
+    def actButton(self):
+        msgbx.showinfo('Action','Has presionado el boton')
